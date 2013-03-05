173c99b132a270402bcaf823cd7edd1e3cdd7035
dschenkelman <damian.schenkelman@gmail.com>
2013-03-05 02:27:44 -0300

    Added initial set of tests for ScriptExecutor

---
+++ src/Scriptcs/Wrappers/ScriptEngineWrapper.cs
@@ -0,0 +1,38 @@
+﻿namespace Scriptcs.Wrappers
+{
+    using Roslyn.Scripting;
+    using Roslyn.Scripting.CSharp;
+
+    internal class ScriptEngineWrapper : IScriptEngine
+    {
+        private ScriptEngine scriptEngine;
+
+        public ScriptEngineWrapper()
+        {
+            this.scriptEngine = new ScriptEngine();
+        }
+
+        public void AddReference(string assemblyDisplayNameOrPath)
+        {
+            this.scriptEngine.AddReference(assemblyDisplayNameOrPath);
+        }
+
+        public ISession CreateSession()
+        {
+            return new SessionWrapper(this.scriptEngine.CreateSession());
+        }
+
+        public string BaseDirectory
+        {
+            get 
+            { 
+                return this.scriptEngine.BaseDirectory; 
+            }
+
+            set 
+            {
+                this.scriptEngine.BaseDirectory = value;
+            }
+        }
+    }
+}
